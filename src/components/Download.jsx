d5d3a7eb3aaec61ada05dd0a8f7bd32ac1f5e2ba
Vanessa Kasminoff <sunnset07@gmail.com>
2024-08-04 16:57:26 -0700

    Modified downloads page

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -5,7 +5,17 @@ const Download = () => {
     return (
         <div>
             <Navbar/>
-            <h1>Downloads Page</h1>
+            <section className="downloads-section">
+                <div className="downloads-content-container">
+                    <h1>Download Anki for Free Today!</h1>
+                    <select>
+                        <option>--Select an Operating System--</option>
+                        <option value="windows">Windows 10/11</option>
+                        <option value="mac">macOS 12+</option>
+                        <option value="linux">Linux (.zst)</option>
+                    </select>
+                </div>
+            </section>
         </div>
     )
 };
